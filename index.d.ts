e5945b670a8145b53cad0efbb67c63c0131c8157
Sasial <44125644+sasial-dev@users.noreply.github.com>
2022-05-22 09:42:42 +1000

    Add TS Typings

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+interface Config {
+	repository: string;
+	tempLocation: string;
+	fromReleases?: boolean;
+	branch?: string;
+	token?: string;
+	ignoreFiles?: string[];
+	executeOnComplete?: string;
+	exitOnComplete: boolean;
+}
+
+export default class AutoGitUpdate {
+	constructor(config: Config);
+	autoUpdate(): Promise<boolean>;
+	compareVersions(): Promise<{
+		upToDate: boolean;
+		currentVersion: string;
+		remoteVersion: string;
+	}>;
+	forceUpdate(): Promise<boolean>;
+}
